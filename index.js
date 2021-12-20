61890de602573642c9146038aa64c5edac0e4850
lowy98 <65040667+lowy98@users.noreply.github.com>
2021-12-20 03:16:20 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,194 @@
+const { WebSocketServer } = require("ws")
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+
+const errorMessage = 'Error has occured'
+/**
+ * 
+ * @param {String} query 
+ * @returns {Object|null}
+ */
+function handleQuery(query) {
+    try {
+        if (query) {
+            let newQuery;
+            newQuery = JSON.parse(
+                '{"' + decodeURIComponent(query)
+                    .replace(/"/, '\'')
+                    .replace(/=/g, '":"')
+                    .replace(/&/g, '","') + '"}'
+            );
+            return newQuery;
+        }
+        else return null
+    }
+    catch {
+        return null;
+    }
+}
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url.match(/\/\d+/))
+    res.setHeader('Content-Security-Policy',
+        "default-src 'self' ws:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' blob:")
+    res.setHeader('Referrer-Policy', 'no-referrer, strict-origin');
+    res.setHeader('X-Frame-Options', 'sameorigin');
+    const url = req.url.match(/^\/([A-Z0-9-]*)/ig)[0];
+    const originalUrl = req.url;
+    let query = !req.url.match(/\?(.+)/) ? null : req.url.match(/\?(.+)/)[1];
+    query = handleQuery(query);
+    if (url === "/") {
+        // res.setHeader('Content-Type', 'text/html');
+        // const htmlFile = fs.readFileSync(path.resolve(__dirname, "views/roomChoosing.html"), 'utf8');
+        // res.setHeader('Content-Length', htmlFile.length);
+        // res.write(htmlFile);
+        // res.end("");
+        res.writeHead(302, {
+            location: "/" + Date.now(),
+        });
+        res.end('')
+    }
+    else if (url.match(/\/\d+$/)) {
+        const roomId = url.match(/\/(\d+)$/)[1];
+        // const role = query['role'];
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(path.resolve(__dirname, "views/index.html")).pipe(res);
+    }
+    else if (url.match(/^\/((?:script)|(?:style))$/)) {
+        const filePath = originalUrl;
+        // if (filePath.match(/(style)$/)) extension = ".css";
+        // else if (filePath.match(/(script)$/)) extension = ".js";
+        try {
+            const file = fs.readFileSync(path.resolve(__dirname, `views` + filePath));
+            if (filePath.match(/\.css$/)) res.setHeader('Content-Type', 'text/css');
+            else if (filePath.match(/\.js$/)) res.setHeader('Content-Type', 'text/javascript');
+            res.setHeader('Content-Length', file.length);
+            res.write(file);
+            res.end('');
+        } catch (e) {
+            console.log(e.message)
+            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Content-Length', errorMessage.length);
+            res.write(errorMessage);
+            res.end('');
+        }
+    }
+    else {
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('Invalid, Error occured');
+        res.end('');
+    }
+})
+server.listen(8000, 'localhost', () => {
+    console.log("Sever Started at", 8000)
+})
+const wss = new WebSocketServer({ server })
+
+const users = {};
+
+
+wss.on('connection', socket => {
+
+    socket.on('message', data => {
+        data = JSON.parse(Buffer.from(data).toString('utf8'));
+        const type = data.type;
+        console.log(users);
+        switch (type) {
+            case "join_create": {
+                if (users[data['room']]) {
+                    // room already exists
+                    if (users[data['room']].others.length >= 2) {
+                        // room is full
+                        sendSocketMessage(socket, {
+                            type: 'full_room',
+                            message: "It's full"
+                        });
+                    }
+                    else {
+                        users[data['room']].others.push({
+                            socket
+                        });
+                        users[data['room']].others.forEach(other => {
+                            other.socket !== socket && sendSocketMessage(other.socket, {
+                                type: 'another_joined',
+                            })
+                        })
+                        sendSocketMessage(socket, {
+                            type: 'i_joined'
+                        })
+                    }
+
+                }
+                else {
+                    // new room
+                    users[data['room']] = {
+                        others: [{
+                            socket,
+                            isInitiator: true
+                        }]
+                    }
+                    sendSocketMessage(socket, {
+                        type: 'created',
+                    })
+                }
+                break;
+            }
+            case 'cannot_get_user_media': {
+                break;
+            }
+            case "disconnect": {
+                removeUser(socket);
+            }
+            default:
+                users[data['room']]?.others.forEach(other => {
+                    other.socket !== socket && sendSocketMessage(other.socket, { ...data });
+                });
+
+        }
+        console.log(data);
+
+
+    });
+
+    socket.on('close', (e) => {
+        try {
+            removeUser(socket);
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    })
+})
+
+function stringifyData(data) {
+    return JSON.stringify(data);
+}
+function removeUser(socket) {
+    const ObjectToArray = Object.entries(users);
+    const [room, usersInRoom] = ObjectToArray.find(o =>
+        o[1]['others'] && o[1]['others'].find(other => other.socket === socket)
+    )
+    // if (usersInRoom['others'].find(other => other.socker === socket && other.isInitiator)) {
+    //     delete users[room];
+    // }
+    const thisUser = usersInRoom['others'].find(other => other.socket === socket);
+    if (thisUser) {
+        const thisUserInDbIndex = usersInRoom['others'].findIndex(other => other.socket === socket)
+        usersInRoom['others'].splice(thisUserInDbIndex, 1);
+        if (usersInRoom['others'].length <= 0) {
+            delete users[room];
+        }
+    }
+}
+/**
+ * 
+ * @param {WebSocket} socket 
+ * @param {Object} data 
+ */
+function sendSocketMessage(socket, data) {
+    socket.send(stringifyData(data));
+}
---
+++ views/script.js
@@ -0,0 +1,310 @@
+/**
+ * @type {RTCPeerConnection}
+ */
+let pc;
+/**
+ * @type {RTCDataChannel}
+ */
+let dc;
+let callerAndCalleeHere;
+/**
+ * @type {MediaStream}
+ */
+let localStream;
+let offer;
+let isStarted = false;
+let isInitiator = false;
+let otherPeerConnected = false;
+let offerSent;
+// Elements
+const videoToggler = document.getElementById('video-toggler');
+const audioToggler = document.getElementById('audio-toggler');
+const messageForm = document.getElementById("form-message");
+const messagesBoxEle = document.getElementsByClassName("message-box__messages")[0];
+
+
+/**
+ * @constant localVideoEle @type {HTMLMediaElement}
+ */
+const localVideoEle = document.getElementById('local-video');
+/**
+ * @constant remoteVideoEle @type {HTMLMediaElement}
+ */
+const remoteVideoEle = document.getElementById('remote-video');
+
+const CONSTRAINTS = {
+    video: true,
+    audio: true
+}
+const ROOM = window.location.pathname.replace(/^\//, "");
+
+
+//Initialize turn/stun server here
+
+const localStreamConstraints = {
+    video: true,
+    audio: true
+}
+
+const ws = new WebSocket("ws://localhost:8000");
+
+
+function sendSocket(data) {
+    data = JSON.stringify({ ...data, room: ROOM });
+    ws.send(data);
+
+}
+
+document.getElementById("id").value = window.location.origin + "/" + ROOM;
+document.querySelectorAll(".copy-to-clipboard").forEach(ele => {
+    ele.addEventListener('click', e => {
+        e.target.previousElementSibling.select();
+        document.execCommand('copy');
+        e.target.previousElementSibling.setSelectionRange(0, 0);
+    })
+})
+ws.addEventListener('open', async e => {
+    window.onbeforeunload = () => {
+        if (ws.readyState === 1)
+            sendSocket({
+                type: "close",
+            })
+        // ws.close();
+    };
+    await getStreamData();
+    messageForm.onsubmit = e => {
+        e.preventDefault();
+        if (!dc) return
+        const inputEle = messageForm['message-box__input'];
+        if (inputEle.value) {
+            dc.send(JSON.stringify({
+                data: inputEle.value.trim()
+            }));
+            handleMessages(dc.label, inputEle.value.trim());
+            inputEle.value = "";
+        }
+    }
+});
+ws.addEventListener('message', async ({ data }) => {
+    data = JSON.parse(data);
+    const type = data.type;
+    switch (type) {
+        case 'created':
+            isInitiator = true;
+            console.log('isInitiator');
+            break;
+        case 'i_joined':
+            break;
+        case 'another_joined':
+            // another peer just joined
+            otherPeerConnected = true;
+            createPeerConnection();
+            break;
+        // case 'user_media_ready':
+        //     createPeerConnection();
+        //     break;
+        case 'full_room':
+            if (localStream) {
+                localStream.getTracks().forEach(track => {
+                    track.stop();
+                })
+                console.log("full")
+                removeStreamFromMediaElement(localStream);
+            }
+        case 'candidate':
+            if (!pc) break;
+            const candidate = new RTCIceCandidate(data.candidate);
+            pc.addIceCandidate(candidate);
+            break;
+        case 'answer':
+            pc.setRemoteDescription(data.answer);
+            break;
+        case 'offer':
+            createPeerConnection();
+            await pc.setRemoteDescription(data.offer);
+            await doAnswer();
+            break
+        case 'disconnect':
+        case 'close':
+            isInitiator = true;
+            closePeerConnection();
+            removeStreamFromMediaElement(remoteVideoEle);
+            break;
+    }
+})
+function createPeerConnection() {
+    pc = new RTCPeerConnection(
+    );
+
+    localStream.getTracks().forEach((track) => {
+        pc.addTrack(track, localStream);
+    });
+    pc.ontrack = handleAddedTrack;
+    pc.onicecandidate = handleIceCandidate;
+    // pc.onnegotiationneeded = console.log
+    // gets called when DataChannel is opened
+    if (isInitiator) {
+        /*** @type {RTCDataChannel}  */
+        dc = pc.createDataChannel('message_channel', { ordered: true, maxPacketLifeTime: 5000 });
+        dc.onopen = e => {
+
+        };
+        pc.ondatachannel = e => {
+            console.log('ondatachannel')
+        };
+        dc.onmessage = msgEvent => {
+            const { data } = JSON.parse(msgEvent.data);
+            handleMessages(dc.label, data, false);
+        };
+        dc.onerror = console.log
+        doCall();
+    }
+    else {
+        pc.ondatachannel = e => {
+            console.log('ondatachannel');
+            dc = e.channel;
+            dc.onmessage = msgEvent => {
+                const { data } = JSON.parse(msgEvent.data);
+                handleMessages(dc.label, data, false);
+            }
+            dc.onerror = console.log
+        }
+
+    }
+}
+/**
+ * 
+ * @param {RTCTrackEvent} e 
+ */
+function handleAddedTrack(e) {
+    e.track.onunmute = unMuteEvent => {
+        // console.log(unMuteEvent);
+        pc.getStats(null).then(stats => {
+            // stats.forEach(console.log)
+        });
+        if (remoteVideoEle.srcObject) return;
+        remoteVideoEle.srcObject = e.streams[0];
+    }
+    e.track.onmute = muteEvent => {
+        // console.log(muteEvent);
+    }
+
+}
+
+function handleMessages(label, message, isMine = true) {
+    switch (label) {
+        case "message_channel":
+            const li = document.createElement("li");
+            li.append(message);
+            li.classList.add("message-box__message");
+            isMine ?
+                li.classList.add("message-box__message--right")
+                : li.classList.add("message-box__message--left");
+            messagesBoxEle.append(li);
+            break;
+    }
+}
+/**
+ * 
+ * @param {RTCPeerConnectionIceEvent} e 
+ */
+function handleIceCandidate(e) {
+    if (e.candidate) {
+        // sendSocket({
+        //     type: "candidate",
+        //     candidate: e.candidate.candidate,
+        //     sdpMLineIndex: e.candidate.sdpMLineIndex,
+        //     sdpMid: e.candidate.sdpMid
+        // })
+
+        sendSocket({
+            type: "candidate",
+            candidate: e.candidate,
+        })
+    }
+
+
+}
+async function doCall() {
+    return new Promise(async res => {
+        offer = await pc.createOffer({
+            offerToReceiveAudio: true,
+            offerToReceiveVideo: true
+        });
+        await pc.setLocalDescription(offer);
+        if (otherPeerConnected) sendSocket({ type: 'offer', offer });
+        res(offer);
+    });
+}
+
+async function doAnswer() {
+    const answer = await pc.createAnswer({
+
+    });
+    await pc.setLocalDescription(answer);
+    sendSocket({ type: "answer", answer });
+
+    return new Promise(res => res(answer));
+}
+
+function closePeerConnection() {
+    if (pc) {
+        pc.getTransceivers().forEach(transceiver => transceiver.stop());
+        pc.close();
+    }
+    if (dc) dc.close();
+    pc = null;
+    dc = null;
+}
+
+/**
+ * 
+ * @returns {Promise<MediaStream>}
+ */
+function getStreamData() {
+    return new Promise(async res => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                    facingMode: {
+                        ideal: ['user', 'environment']
+                    }
+                }
+            });
+            localStream = stream;
+            localVideoEle.srcObject = stream;
+            localStream.getVideoTracks()[0].onended = e => {
+                sendSocket({
+                    type: "disconnect"
+                });
+            };
+            sendSocket({
+                type: "join_create",
+            });
+            videoToggler.addEventListener('click', e => {
+                toggleVideo();
+            });
+            audioToggler.addEventListener('click', e => {
+                toggleAudio();
+            })
+            res(stream);
+        } catch (e) {
+            console.log(e.message)
+            sendSocket({
+                type: 'user_media_error',
+                message: e.message || ''
+            });
+        }
+    })
+}
+function toggleVideo() {
+    if (localStream)
+        localStream.getVideoTracks().forEach(track => track.enabled = !track.enabled)
+}
+function toggleAudio() {
+    if (localStream)
+        localStream.getAudioTracks().forEach(track => track.enabled = !track.enabled)
+}
+function removeStreamFromMediaElement(videoElement) {
+    videoElement.srcObject = null;
+}
